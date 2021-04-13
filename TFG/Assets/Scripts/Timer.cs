ea5a60b392cbe8a4055a101629b95bf3bf101f19
1426841 <oriol.puigdomenech@e-campus.uab.cat>
2021-04-13 18:48:46 +0200

    Timer
    Creada la classe Timer que compte el temps total en el nivell.

---
+++ TFG/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text timerText;
+    private float initialTime;
+    private float totalTime;
+
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+        initialTime = Time.time;
+        totalTime = 0;
+    }
+
+    public float GetTotalTime()
+    {
+        return totalTime;
+    }
+
+    void Update()
+    {
+        totalTime = Time.time - initialTime;
+
+        timerText.text = totalTime.ToString();
+    }
+}
---
+++ TFG/Assets/Tests/PlayMode/Timer Tests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class TimerTests
+    {
+        
+        [UnityTest]
+        public IEnumerator CountTotalTime()
+        {
+            var gameObject = new GameObject();
+            var timer = gameObject.AddComponent<Timer>();
+            timer.gameObject.AddComponent<Text>();
+
+            Assert.AreEqual(timer.GetTotalTime(), 0);
+
+            yield return new WaitForSeconds(1);
+            Assert.AreEqual((int) timer.GetTotalTime(), 1);
+
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(timer.GetTotalTime().ToString("f1"), "1,5");
+        }
+    }
+}
