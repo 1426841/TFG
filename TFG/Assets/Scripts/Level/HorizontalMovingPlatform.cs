b711ae320e307a5dfd7398988969a45e2620d423
1426841 <oriol.puigdomenech@e-campus.uab.cat>
2021-04-27 19:17:43 +0200

    HorizontalMovingPlatform
    El primer nivell ja té una plataforma que es mou horitzonalment.

---
+++ TFG/Assets/Scripts/Level/HorizontalMovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class HorizontalMovingPlatform : MonoBehaviour
+{
+    private const float PlatformSpeed = 3;
+
+    public Transform InitialPosition;
+    public Transform FinalPosition;
+    private Vector3 movePosition;
+
+    void Start()
+    {
+        movePosition = InitialPosition.position;   
+    }
+
+    void Update()
+    {
+        if (transform.position == InitialPosition.position)
+        {
+            movePosition = FinalPosition.position;
+        } else if (transform.position == FinalPosition.position)
+        {
+            movePosition = InitialPosition.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, movePosition, PlatformSpeed*Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+}
---
+++ TFG/Assets/Tests/PlayMode/HorizontalMovingPlatformTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HorizontalMovingPlatformTests
+    {   
+        [UnityTest]
+        public IEnumerator HorizontalMovingPlatForm()
+        {
+            var platformObject = new GameObject();
+            var platform = platformObject.AddComponent<HorizontalMovingPlatform>();
+
+            GameObject initialObject = new GameObject();
+            Transform initialTransform = initialObject.transform;
+            platform.InitialPosition = initialTransform;
+            GameObject finalObject = new GameObject();
+            Transform finalTransform = finalObject.transform;
+            platform.FinalPosition =  finalTransform;
+            
+            platform.InitialPosition.position = new Vector3(-3, 0);
+            platform.FinalPosition.position = new Vector3(3, 0);
+
+            float originalPosition = platform.transform.position.x;
+
+            yield return new WaitForSeconds(1);
+            Assert.Less(platform.transform.position.x, originalPosition);
+
+            yield return new WaitForSeconds(1);
+            Assert.AreEqual((int)platform.transform.position.x, (int)originalPosition);
+
+            yield return new WaitForSeconds(1);
+            Assert.Greater(platform.transform.position.x, originalPosition);
+
+            yield return new WaitForSeconds(1);
+            Assert.AreEqual((int)platform.transform.position.x,(int) originalPosition);
+        }
+    }
+}
