8102d1b4ac02fd6a3735537a38a99acfbc9565f1
1426841 <oriol.puigdomenech@e-campus.uab.cat>
2021-05-13 20:13:16 +0200

    AudioSettings SetVolume
    El joc permet configurar el nivell d'àudio.

---
+++ TFG/Assets/Scripts/Settings/AudioSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", Mathf.Log10(volume) * 20);
+    }
+}
