536ea1e5479e8f9457f6a5b7875c66970837624b
1426841 <oriol.puigdomenech@e-campus.uab.cat>
2021-05-13 19:34:55 +0200

    GraphicsSettings SetResolution
    El joc permet elegir la resolució.

--- TFG/Assets/Scripts/Settings/GraphicsSettings.cs
+++ TFG/Assets/Scripts/Settings/GraphicsSettings.cs
@@ -1,11 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class GraphicsSettings : MonoBehaviour
 {
     public Toggle toggle;
     public EventSystem eventSystem;
+    public Dropdown resolutionsDropdown;
+    private Resolution[] screenResolutions;
+
+    private void Start()
+    {
+        resolutionsDropdown.ClearOptions();
+        screenResolutions = Screen.resolutions;
+        List<string> optionsDropdown = new List<string>();
+        int currentResolution = 0;
+
+        for (int i = 0; i < screenResolutions.Length; i++)
+        {
+            if((screenResolutions[i].refreshRate == 59) || (screenResolutions[i].refreshRate == 60))
+            {
+                optionsDropdown.Add(screenResolutions[i].ToString());
+            }
+            
+            if(screenResolutions[i].width == Screen.width && screenResolutions[i].height == Screen.height && 
+                (screenResolutions[i].refreshRate == Screen.currentResolution.refreshRate || screenResolutions[i].refreshRate+1 == Screen.currentResolution.refreshRate))
+            {
+                currentResolution = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(optionsDropdown);
+        resolutionsDropdown.value = currentResolution;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
     public void Fullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
@@ -15,5 +45,11 @@ public class GraphicsSettings : MonoBehaviour
     {
         eventSystem.SetSelectedGameObject(null);
         eventSystem.SetSelectedGameObject(toggle.gameObject);
+    }
+
+    public void SetResolution(int selectedResolution)
+    {
+        Resolution resolution = screenResolutions[selectedResolution];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
