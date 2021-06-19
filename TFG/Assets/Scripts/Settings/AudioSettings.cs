574e7d41d32b24f53cc8a9cd77714a3db2a8fc1e
1426841 <oriol.puigdomenech@e-campus.uab.cat>
2021-06-19 14:42:29 +0200

    Volume PlayerPrefs
    El volum de l'àudio es guarda en PlayerPrefs.

--- TFG/Assets/Scripts/Settings/AudioSettings.cs
+++ TFG/Assets/Scripts/Settings/AudioSettings.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider slider;
+
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("Volume", 1);
+    }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("masterVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 }
